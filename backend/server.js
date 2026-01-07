5af3b0e1c2b8823f7ff069aecd1a40086dbc5885
Mercy Ogalo <ogalomercy8@gmail.com>
2026-01-07 22:54:10 +0300

    Created the backend setup

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const cors=require("cors");
+const dotenv=require("dotenv");
+dotenv.config();
+
+const app=express();
+app.use(cors());
+
+
+const PORT=process.env.PORT || 5000;
+
+mongoose
+.connect(process.env.MONGO_URI,{})
+.then(()=>console.log("Mongodb running"))
+.catch((err)=>console.log("Mongodb error is:",err))
+
+app.listen(PORT=>{
+    console.log(`Server running on port ${PORT}`)
+})
+
+
+
